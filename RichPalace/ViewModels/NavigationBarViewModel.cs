2ca9bf586cbe1edca0e321473b64434e72cd9c56
lybitechokoladok <102763296+lybitechokoladok@users.noreply.github.com>
2023-02-12 23:18:49 +0300

    littel changes

--- RichPalace/ViewModels/NavigationBarViewModel.cs
+++ RichPalace/ViewModels/NavigationBarViewModel.cs
@@ -17,7 +17,7 @@ namespace RichPalace.WPF.ViewModels
         public ICommand NavigateHomeCommand { get; }
         public ICommand NavigateAccountCommand { get; }
         public ICommand NavigateLoginCommand { get; }
-        public ICommand NavigatePeopleListingCommand { get; }
+        public ICommand NavigateClientListingCommand { get; }
 
         public ICommand NavigateReservationListingCommand { get; }
         public ICommand LogoutCommand { get; }
@@ -29,14 +29,14 @@ namespace RichPalace.WPF.ViewModels
             INavigationService accountNavigationService,
             INavigationService reservationListingNavigationService,
             INavigationService loginNavigationService,
-            INavigationService peopleListingNavigationService)
+            INavigationService clientListingNavigationService)
         {
             _accountStore = accountStore;
             NavigateReservationListingCommand = new NavigateCommand(reservationListingNavigationService);
             NavigateHomeCommand = new NavigateCommand(homeNavigationService);
             NavigateAccountCommand = new NavigateCommand(accountNavigationService);
             NavigateLoginCommand = new NavigateCommand(loginNavigationService);
-            NavigatePeopleListingCommand = new NavigateCommand(peopleListingNavigationService);
+            NavigateClientListingCommand = new NavigateCommand(clientListingNavigationService);
             LogoutCommand = new LogoutCommand(_accountStore);
 
             _accountStore.CurrentAccountChanged += OnCurrentAccountChanged;
